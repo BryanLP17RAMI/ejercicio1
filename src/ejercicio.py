80bc76f48b94e3ef500731baa17d51e84e436ef8
BRIAN-FERNANDEZ <BRIAN-FERNANDEZ@BRIAN>
2015-10-19 17:23:21 -0500

    ejercicios phyton

---
+++ src/ejercicio.py
@@ -0,0 +1,15 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "BRIAN-FERNANDEZ"
+__date__ = "$19/10/2015 05:22:31 PM$"
+
+#calcular si un numero es par o inpar :D
+
+num=int(input("Introdusca valor del numero: "))
+if num % 2 == 0:
+    print "el numero es par"
+else:
+    print "El numero es inpar"
+    
